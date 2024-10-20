b8ef451e3ad7ae06bd375414a00a9f0b82195b9f
AfifRizqiRamadhan <afif.rizki09@gmail.com>
2024-10-20 23:42:55 +0800

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,29 @@
+#program daftar nama produk beserta merk dan harganya
+#produk pulpen
+nama = "pulpen"
+merk = "standart"
+harga = "4.000"
+hasil_string = "nama: " + nama + ", merk: " + merk + ", harga: " + harga
+
+#produk pensil
+nama = "pensil"
+merk = "castel"
+harga = "3.000"
+hasil_string1 = "nama: " + nama + ", merk: " + merk + ", harga: " + harga
+
+#produk stip-x
+nama = "stip-x"
+merk = "kenko"
+harga = "6.000"
+hasil_string2 = "nama: " + nama + ", merk: " + merk + ", harga: " + harga
+
+#produk penghapus
+nama = "penghapus"
+merk = "joyco"
+harga = "5.000"
+hasil_string3 = "nama: " + nama + ", merk: " + merk + ", harga: " + harga
+
+print("daftar produk: ", hasil_string)
+print("daftar produk: ", hasil_string1)
+print("daftar produk: ", hasil_string2)
+print("daftar produk: ", hasil_string3)
